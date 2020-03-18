e226853a694603178a8031acf02454ed5c891501
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-03-18 21:30:02 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES( "Mr. Chocolate", 2, "male", "brown", "sweet", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES( "Rowdy", 4, "female", "black", "sweet", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES( "Tabitha", 3, "male", "brown", "sweet", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES( "Sergeant Brown", 12, "male", "brown", "angry", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES( "Melissa", 8, "female", "black", "sweet", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES( "Grinch", 6, "male", "green", "stealer", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES( "Wendy", 2, "female", "white", "sweet", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES( NULL, 2, "male", "brown", "hungry", 0);
